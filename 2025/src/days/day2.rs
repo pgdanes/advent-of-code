697c224fc87bce86ca34556eaebf4b439e4179a5
pgdanes <petergdanes@gmail.com>
2025-12-02 14:14:06 +0800

    day2 b

--- 2025/src/days/day2.rs
+++ 2025/src/days/day2.rs
@@ -3,17 +3,6 @@ pub struct IdRange {
     end: i64
 }
 
-pub fn is_invalid(id: &str) -> bool {
-    let length = id.len();
-    if length % 2 != 0 {
-        return false;
-    }
-
-    let mid = length/2;
-    let left = &id[..mid];
-    let right = &id[mid..];
-    left == right
-}
 
 pub fn parse(s: String) -> Vec<IdRange> {
     s.trim()
@@ -28,25 +17,64 @@ pub fn parse(s: String) -> Vec<IdRange> {
         .collect()
 }
 
-mod part1 {
-    use super::*;
+pub fn solve(s: String, invalid_fn: &dyn Fn(&str) -> bool) -> i64 {
+    let ranges = parse(s);
 
-    pub fn solve(s: String) -> i64 {
-        let ranges = parse(s);
+    let mut total = 0;
 
-        let mut total = 0;
-
-        ranges
-            .iter()
-            .for_each(|range| {
-                for i in range.start..=range.end {
-                    if is_invalid(&i.to_string()) {
-                        total += i
-                    }
+    ranges
+        .iter()
+        .for_each(|range| {
+            for i in range.start..=range.end {
+                if invalid_fn(&i.to_string()) {
+                    total += i
                 }
-            });
+            }
+        });
 
-        total
+    total
+}
+
+mod part1 {
+    pub fn is_invalid(id: &str) -> bool {
+        let length = id.len();
+        if length % 2 != 0 {
+            return false;
+        }
+
+        let mid = length/2;
+        let left = &id[..mid];
+        let right = &id[mid..];
+        left == right
+    }
+}
+
+mod part2 {
+    use std::cmp::min;
+
+    pub fn is_invalid(id: &str) -> bool {
+        let length = id.len();
+        let mid = length/2;
+
+        for part_amount in 1..=mid {
+            let mut start = 0;
+            let end = min(start+part_amount, length);
+            let first = &id[start..end];
+            let mut all_match = true;
+
+            while start < length {
+                let end = min(start+part_amount, length);
+                let part = &id[start..end];
+                all_match &= part == first;
+                start += part_amount;
+            }
+
+            if all_match {
+                return true;
+            }
+        }
+
+        false
     }
 }
 
@@ -56,33 +84,72 @@ mod tests {
     use crate::days::read::*;
 
     #[test]
-    fn is_invalid_test() {
-        assert_eq!(is_invalid("11"), true);
-        assert_eq!(is_invalid("22"), true);
-        assert_eq!(is_invalid("99"), true);
-        assert_eq!(is_invalid("1010"), true);
-        assert_eq!(is_invalid("222222"), true);
-        assert_eq!(is_invalid("123123"), true);
-        assert_eq!(is_invalid("1188511885"), true);
-        assert_eq!(is_invalid("38593859"), true);
-
-        assert_eq!(is_invalid("10"), false);
-        assert_eq!(is_invalid("101"), false);
-        assert_eq!(is_invalid("1011"), false);
-        assert_eq!(is_invalid("123"), false);
-    }
-
-    #[test]
     fn part1_example() {
         let input = read_day_example("2").unwrap();
-        let result = part1::solve(input);
+        let result = solve(input, &part1::is_invalid);
         assert_eq!(result, 1227775554);
     }
 
     #[test]
+    #[ignore = "for quicker runs"]
     fn part1_real() {
         let input = read_day("2").unwrap();
-        let result = part1::solve(input);
+        let result = solve(input, &part1::is_invalid);
         assert_eq!(result, 38437576669);
+    }
+
+    #[test]
+    fn part2_example() {
+        let input = read_day_example("2").unwrap();
+        let result = solve(input, &part2::is_invalid);
+        assert_eq!(result, 4174379265);
+    }
+
+    #[test]
+    #[ignore = "for quicker runs"]
+    fn part2_real() {
+        let input = read_day("2").unwrap();
+        let result = solve(input, &part2::is_invalid);
+        assert_eq!(result, 49046150754);
+    }
+
+    #[test]
+    fn part1_is_invalid_test() {
+        assert_eq!(part1::is_invalid("11"), true);
+        assert_eq!(part1::is_invalid("22"), true);
+        assert_eq!(part1::is_invalid("99"), true);
+        assert_eq!(part1::is_invalid("1010"), true);
+        assert_eq!(part1::is_invalid("222222"), true);
+        assert_eq!(part1::is_invalid("123123"), true);
+        assert_eq!(part1::is_invalid("1188511885"), true);
+        assert_eq!(part1::is_invalid("38593859"), true);
+
+        assert_eq!(part1::is_invalid("10"), false);
+        assert_eq!(part1::is_invalid("101"), false);
+        assert_eq!(part1::is_invalid("1011"), false);
+        assert_eq!(part1::is_invalid("123"), false);
+    }
+
+    #[test]
+    fn part2_is_invalid_test() {
+        assert_eq!(part2::is_invalid("11"), true);
+        assert_eq!(part2::is_invalid("22"), true);
+        assert_eq!(part2::is_invalid("99"), true);
+        assert_eq!(part2::is_invalid("1010"), true);
+        assert_eq!(part2::is_invalid("222222"), true);
+
+        assert_eq!(part2::is_invalid("10"), false);
+        assert_eq!(part2::is_invalid("101"), false);
+        assert_eq!(part2::is_invalid("1011"), false);
+        assert_eq!(part2::is_invalid("123"), false);
+
+        // // new ones
+        assert_eq!(part2::is_invalid("111"), true);
+        assert_eq!(part2::is_invalid("11111"), true);
+        assert_eq!(part2::is_invalid("121212"), true);
+        assert_eq!(part2::is_invalid("446446"), true);
+        assert_eq!(part2::is_invalid("38593859"), true);
+        assert_eq!(part2::is_invalid("2121212121"), true);
+        assert_eq!(part2::is_invalid("2121212118"), false);
     }
 }
