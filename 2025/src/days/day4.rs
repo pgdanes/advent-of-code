9caa750c644e899a131eb44e7d1818445ccde0a0
pgdanes <petergdanes@gmail.com>
2025-12-04 14:52:53 +0800

    only recheck neighbours of previously removed

--- 2025/src/days/day4.rs
+++ 2025/src/days/day4.rs
@@ -1,6 +1,7 @@
 use std::collections::HashSet;
 
-type Grid = HashSet<(isize, isize)>;
+type Coord = (isize, isize);
+type Grid = HashSet<Coord>;
 
 pub fn parse(s: String) -> Grid {
     let mut grid = HashSet::new();
@@ -21,7 +22,7 @@ pub fn parse(s: String) -> Grid {
     grid
 }
 
-static ADJACENCIES: &'static [(isize, isize)] = &[
+static ADJACENCIES: &'static [Coord] = &[
     (-1,-1),
     (-1,0),
     (-1,1),
@@ -32,7 +33,8 @@ static ADJACENCIES: &'static [(isize, isize)] = &[
     (1,1),
 ];
 
-pub fn count_neighbours(grid: &Grid, coord: (isize, isize)) -> u32 {
+
+pub fn count_neighbours(grid: &Grid, coord: Coord) -> u32 {
     ADJACENCIES
         .iter()
         .map(|adjacency| {
@@ -48,7 +50,7 @@ pub fn count_neighbours(grid: &Grid, coord: (isize, isize)) -> u32 {
 mod part1 {
     use super::*;
 
-    pub fn solve(s: String) -> u32 {
+    pub fn solve(s: String) -> usize {
         let grid = parse(s);
 
         grid.iter()
@@ -62,26 +64,41 @@ mod part1 {
 mod part2 {
     use super::*;
 
-    pub fn solve(s: String) -> u32 {
+    pub fn solve(s: String) -> usize {
         let mut grid = parse(s);
 
-        let mut removed = 1;
-        let mut total: u32 = 0;
+        let mut removable_set = HashSet::<Coord>::new();
+        let mut total = 0;
 
-        while removed > 0 {
-            let found: Vec<(isize, isize)> = grid
-                .iter()
-                .flat_map(|&paper_coord| {
-                    let count = count_neighbours(&grid, paper_coord) ;
-                    if count < 4 { Some(paper_coord.to_owned()) } else { None }
-                })
-                .collect();
+        for &paper_coord in grid.iter() {
+            let count = count_neighbours(&grid, paper_coord);
+            if count < 4 {
+                removable_set.insert(paper_coord);
+            }
+        }
 
-            removed = found.iter().count() as u32;
-            total += removed;
+        while !removable_set.is_empty() {
+            let to_remove: Vec<Coord> = removable_set.drain().collect();
+            let removed_count = to_remove.len();
+            let mut neighbours_to_recheck = HashSet::<Coord>::new();
+            total += removed_count;
 
-            for paper_coord in found {
-                grid.remove(&paper_coord);
+            for coord_to_remove in to_remove {
+                grid.remove(&coord_to_remove);
+
+                for adj in ADJACENCIES.iter() {
+                    let neighbour = (adj.0 + coord_to_remove.0, adj.1 + coord_to_remove.1);
+                    neighbours_to_recheck.insert(neighbour);
+                }
+            }
+
+            for neighbour in neighbours_to_recheck.into_iter() {
+                if !grid.contains(&neighbour) { continue };
+
+                let count = count_neighbours(&grid, neighbour);
+                if count < 4 {
+                    removable_set.insert(neighbour);
+                }
             }
         }
 
